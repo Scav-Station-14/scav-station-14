6fb21d27e3a10ce0184418837f7ee1b9073c5150
markoatonc <atonckeram@gmail.com>
2025-07-30 23:56:38 +0200

    made who work in chat window

--- Content.Server/Commands/WhoCommand.cs
+++ Content.Server/Commands/WhoCommand.cs
@@ -1,23 +1,19 @@
 using System.Text;
 using Robust.Server.Player;
-using System;
-using System.Linq;
 using Robust.Shared.Console;
-using Robust.Shared.IoC;
-using Robust.Shared.Network;
+using Content.Server.Chat.Managers;
+using Content.Shared.Chat;
 
 namespace Content.Server.Commands;
 
 public sealed class Who : LocalizedCommands
 {
     [Dependency] private readonly IPlayerManager _players = default!;
-
+    [Dependency] private readonly IChatManager _chat = default!;
     public override string Command => "who";
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        // Player: number of people connected and their byond keys
-        // EDIT: inb4 sued by MSO for AGPLv3 violation.
-
+        //copied from listplayers command but without the IPs and avalible to all players
         var sb = new StringBuilder();
 
         var players = _players.Sessions;
@@ -34,6 +30,11 @@ public sealed class Who : LocalizedCommands
         }
 
         shell.WriteLine(sb.ToString());
+        if (shell.Player!=null)
+        {
+            _chat.DispatchServerMessage(shell.Player, sb.ToString());
+        }
+
     }
 
 }
