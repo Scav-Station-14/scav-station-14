0f40009f42627a61a361fbafe346ef3529162f72
markoatonc <atonckeram@gmail.com>
2025-07-30 21:44:52 +0200

    Who command

---
+++ Content.Server/Commands/WhoCommand.cs
@@ -0,0 +1,40 @@
+using System.Text;
+using Robust.Server.Player;
+using System;
+using System.Linq;
+using Robust.Shared.Console;
+using Robust.Shared.IoC;
+using Robust.Shared.Network;
+
+namespace Content.Server.Commands;
+
+public sealed class Who : LocalizedCommands
+{
+    [Dependency] private readonly IPlayerManager _players = default!;
+
+    public override string Command => "who";
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        // Player: number of people connected and their byond keys
+        // EDIT: inb4 sued by MSO for AGPLv3 violation.
+
+        var sb = new StringBuilder();
+
+        var players = _players.Sessions;
+        sb.AppendLine($"{"Player Name",20} {"Status",12} {"Playing Time",14} {"Ping",9}");
+        sb.AppendLine("--------------------------------------------------------------");
+
+        foreach (var p in players)
+        {
+            sb.AppendLine(string.Format("{3,20} {0,12} {1,14:hh\\:mm\\:ss} {2,9}",
+                p.Status.ToString(),
+                DateTime.UtcNow - p.ConnectedTime,
+                p.Channel.Ping + "ms",
+                p.Name));
+        }
+
+        shell.WriteLine(sb.ToString());
+    }
+
+}
+
---
+++ Resources/Locale/en-US/commands/who-command.ftl
@@ -0,0 +1,2 @@
+cmd-who-desc = Lists all connected Players
+cmd-who-help = Lists all connected Players along with their status, uptime and ping
