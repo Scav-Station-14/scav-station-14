23086339d236f606b6ec058fa20c55e23f2b3366
NuclearCreeperIII <siahborg@gmail.com>
2026-02-25 19:04:49 -0500

    Added stationmapprototype, added station table to database

---
+++ Content.Server.Database/Migrations/Postgres/20260225235940_station_1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class station_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "station",
+                columns: table => new
+                {
+                    station_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    station_name = table.Column<string>(type: "text", nullable: false),
+                    station_prototype_id = table.Column<string>(type: "text", nullable: false),
+                    bank_balance = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_station", x => x.station_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "station");
+        }
+    }
+}
---
+++ Content.Server.Database/Migrations/Sqlite/20260225235919_station_1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class station_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "station",
+                columns: table => new
+                {
+                    station_id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    station_name = table.Column<string>(type: "TEXT", nullable: false),
+                    station_prototype_id = table.Column<string>(type: "TEXT", nullable: false),
+                    bank_balance = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_station", x => x.station_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "station");
+        }
+    }
+}
