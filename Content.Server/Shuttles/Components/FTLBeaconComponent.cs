12d55aed397991a424e375f1639f311869b1fae4
NuclearCreeperIII <siahborg@gmail.com>
2026-04-15 19:49:02 -0400

    Fixed expeditions missing their landing offset

--- Content.Server/Shuttles/Components/FTLBeaconComponent.cs
+++ Content.Server/Shuttles/Components/FTLBeaconComponent.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Content.Server.Shuttles.Components;
 
 /// <summary>
@@ -6,5 +8,15 @@ namespace Content.Server.Shuttles.Components;
 [RegisterComponent]
 public sealed partial class FTLBeaconComponent : Component
 {
+    /// <summary>
+    /// Coords to use when FTLing to a destination. Used to ensure that landing is outside of a dungeon, for example.
+    /// </summary>
+    [DataField]
+    public Vector2 Coords = Vector2.Zero;
 
+    /// <summary>
+    /// Angle rotation used to apply the shuttle offset relative to.
+    /// </summary>
+    [DataField]
+    public Angle Rotation = Angle.Zero;
 }
